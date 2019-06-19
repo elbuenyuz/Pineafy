6a40924c9ed5f9e0ed0705ddb61f732945891dda
juanmeza <juanmeza255@hotmail.com>
2019-06-18 23:23:27 -0700

    Continue add labels, textViews and Buttons in ViewController

--- Pineafy/View/CustomViews/signInVC.swift
+++ Pineafy/View/CustomViews/signInVC.swift
@@ -13,6 +13,7 @@ class signInVC: UIViewController {
     let phoneTextView: UITextView = {
         
         let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
         textView.isScrollEnabled = false
         return textView
     }()
@@ -20,18 +21,21 @@ class signInVC: UIViewController {
     let emailTextView: UITextView = {
         
         let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
         textView.isScrollEnabled = false
         return textView
     }()
     
     let phoneTitle: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Phone number"
         return label
     }()
     
     let emailTitle: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Email"
         return label
     }()
@@ -39,6 +43,7 @@ class signInVC: UIViewController {
     let getStartedButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor.red
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("GET STARTED", for: [])
         return button
     }()
@@ -46,6 +51,7 @@ class signInVC: UIViewController {
     let facebookButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor.blue
+        button.translatesAutoresizingMaskIntoConstraints = false 
         button.setTitle("SIGN UP WITH FACEBOOK", for: [])
         return button
     }()
@@ -53,10 +59,44 @@ class signInVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupInputsContainerView()
     }
     
-
+    
+    
+    func setupInputsContainerView() {
+        
+        phoneTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        phoneTitle.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        phoneTitle.widthAnchor.constraint(equalToConstant: 21).isActive = true
+        
+        phoneTextView.topAnchor.constraint(equalTo: phoneTitle.bottomAnchor, constant: 8).isActive = true
+        phoneTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        phoneTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 16).isActive = true
+        phoneTextView.widthAnchor.constraint(equalToConstant: 21).isActive = true
+        
+        emailTitle.topAnchor.constraint(equalTo: phoneTextView.bottomAnchor, constant: 16).isActive = true
+        emailTitle.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        emailTitle.widthAnchor.constraint(equalToConstant: 21).isActive = true
+        
+        emailTextView.topAnchor.constraint(equalTo: emailTitle.bottomAnchor, constant: 8).isActive = true
+        emailTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        emailTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 16).isActive = true
+        emailTextView.widthAnchor.constraint(equalToConstant: 21).isActive = true
+        
+        getStartedButton.topAnchor.constraint(equalTo: emailTextView.bottomAnchor, constant: 20).isActive = true
+        getStartedButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        getStartedButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 16).isActive = true
+        getStartedButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        
+        facebookButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 30).isActive = true
+        facebookButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        facebookButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 16).isActive = true
+        facebookButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        
+    }
     /*
     // MARK: - Navigation
 
